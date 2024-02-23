fbd833e8b310449a5023976694b52bbb244f2cbc
GAUDILL1 <GAUDILL1@FRLAPP143>
2024-02-23 14:39:25 +0100

    update sonar 5

--- src/main/java/com/ipi/jva350/model/Entreprise.java
+++ src/main/java/com/ipi/jva350/model/Entreprise.java
@@ -68,25 +68,11 @@ public final class Entreprise {
         );
     }
 
-    public static boolean bissextile(int y) {
+    public static boolean bissextile(int y)
+    {
         String tmp = String.valueOf(y);
-        if (tmp.charAt(2) == '1' || tmp.charAt(2) == '3' || tmp.charAt(2) == 5 || tmp.charAt(2) == '7' || tmp.charAt(2) == '9') {
-            if (tmp.charAt(3)=='2'||tmp.charAt(3)=='6') return true;
-            else
-                return false;
-        }else{
-            if (tmp.charAt(2) == '0' && tmp.charAt(3) == '0') {
-                return false;
-            }
-            if (tmp.charAt(3)=='0'||tmp.charAt(3)=='4'||tmp.charAt(3)=='8')return true;
-        }
-        return false;
+        return (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0));
     }
-
-
-
-
-
 
     //Ajout du cas pour le mois de janvier (01) qui n'est sinon jamais pris en compte dans la méthode.
     public static double proportionPondereeDuMois(LocalDate moisDuConge) {
@@ -140,7 +126,6 @@ public final class Entreprise {
                 : d.getMonthValue() > 5 ? LocalDate.of(d.getYear(), 6, 1)
                 : LocalDate.of(d.getYear() - 1, 6, 1);
     }
-
 
     public static boolean estJourFerie(LocalDate jour) {
         int monEntier = (int) Entreprise.joursFeries(jour).stream().filter(d ->
